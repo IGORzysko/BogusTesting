2defaf2b527b224527b0754a3c49a92e20cd0bf3
Igor Trafalski <igorek2691@gmail.com>
2017-07-25 20:09:08 +0200

    commit

---
+++ UnitTestingWithBogusLibrary/Person.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace UnitTestingWithBogusLibrary
+{
+    public class Person
+    {
+        public Person()
+        {
+        }
+
+        public Person(string name, string surname)
+        {
+            this.Name = name;
+            this.Surname = surname;
+        }
+
+        public Person(string name, string surname, string address)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Address = address;
+        }
+
+        public enum Gender
+        {
+            Male,
+            Female
+        }
+
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Surname { get; set; }
+        [Required]
+        public Gender Sex { get; set; }
+
+        public string Address { get; set; }
+
+        public void DisplayData()
+        {
+            Console.WriteLine($"{Name}, {Surname}, {Sex}, {Address}");
+        }
+    }
+}
---
+++ UnitTestsBogus/UnitTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bogus;
+
+namespace UnitTestsBogus
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            #region Arrange
+
+            var persons = new Faker<UnitTestingWithBogusLibrary.Person>()
+                .StrictMode(false)
+                .CustomInstantiator(f => new UnitTestingWithBogusLibrary.Person())
+                .Rules((f, o) =>
+                {
+                    o.Name = f.Name.FirstName();
+                    o.Surname = f.Name.LastName();
+                    o.Sex = f.PickRandom<UnitTestingWithBogusLibrary.Person.Gender>();
+                });
+
+            #endregion
+
+            #region Act
+
+            var person = persons.Generate(5);
+
+            #endregion
+
+            #region Assert
+
+            Assert.IsNotNull(person);
+
+            #endregion
+
+        }
+    }
+}
